ab3884eec3754fc31518deaa08a2a47996ec8f1e
LAYTHJABBAR <53024934+LAYTHJABBAR@users.noreply.github.com>
2024-07-30 09:33:38 -0600

    update the mongo string to have the sls

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    MONGO_URI = 'mongodb+srv://laythalshareefi:kPDHmAODFvV8Ma0r@cluster0.drlb20b.mongodb.net/test?retryWrites=true&w=majority'
+    MONGO_URI = 'mongodb+srv://laythalshareefi:kPDHmAODFvV8Ma0r@cluster0.drlb20b.mongodb.net/?retryWrites=true&w=majority&tlsCAFile=isrgrootx1.pem'
     MONGO_DBNAME = 'courses_db'
